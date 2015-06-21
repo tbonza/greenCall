7a927cedc5bde9646670646560be5df80e62ea11
tbonza <tylers.pile@gmail.com>
2015-06-20 23:16:34 -0400

    added prelim example & test data

--- examples/okgo.py
+++ examples/okgo.py
@@ -1,7 +1,28 @@
+import json
+import codecs
+import logging
 
-from greencall.clients import googleSearch
+from twisted.internet import reactor
+
+#from greencall.csvclean import goodData
+#from greencall.database import asyncMongo
+#from greencall.clients import googleSearch
+from greencall.crawlah import getPages
+from greencall.utils.utilityBelt import enable_log
 
 
-if name == '__main__':
-    run_async_loop(csv_file=file,
-                   client=googleSearch)
+if __name__ == '__main__':
+    
+    enable_log('crawlah')
+
+    with open('examples/testkitten.json', 'r') as infile:
+        testkitten = json.load(infile)
+
+    for key in testkitten.keys():
+        testkitten[key] = codecs.encode(testkitten[key])
+    
+
+    logging.info("crawlah STARTED")
+    gp = getPages(testkitten)
+    gp.start()
+    reactor.run()
